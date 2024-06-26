cb34127a4d505ebec20fbd56ab64bc51d42d1942
SwethaGitRepo <sganap12@asu.com>
2024-06-26 02:05:31 -0700

    Added schema and resolver code

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,12 +1,17 @@
+import { getCompany } from './db/companies.js';
+import { getJobs } from './db/jobs.js';
+
 export const resolvers = {
-Query: {
-    jobs: () => {
-        return [{
-            id : 'test-id',
-            title: 'The Title',
-            description: 'The description.',
-        },
-    ];
-    },
-},
+  Query: {
+    jobs: () => getJobs(),
+  },
+  
+  Job: {
+    company: (job) => getCompany(job.companyId),
+    date: (job) => toIsoDate(job.createdAt),
+  },
 };
+
+function toIsoDate(value) {
+  return value.slice(0, 'yyyy-mm-dd'.length);
+}
